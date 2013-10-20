91aa3d3ecc74f9a823687405e8346e66aec7e8b8
Cale Bierman <cbierman@Cales-MacBook-Pro.local>
2013-10-20 11:18:21 -0500

    Added button class and formed new button to test

--- SecondGo.py
+++ SecondGo.py
@@ -1,5 +1,37 @@
+
+
 import pygame, sys
 from pygame.locals import *
+
+import pygame
+
+class Button():
+
+    def __init__(self, width, height, x, y, color, text, textColor):
+        self.width = width
+        self.height = height
+        self.xpos = x
+        self.ypos = y
+        # Make a new surface object for the text to live on
+        surface = pygame.Surface((width,height))
+        self.color = color
+        # Load the font with the font size
+        fonts = pygame.font.Font(None, 24)
+        # Render the text
+        displayText = fonts.render(text, 1, color)
+        # Get the size of the rectangle the text is in
+        textpos = displayText.get_rect()
+        # Center the text within it's container
+        textpos.centerx = surface.get_rect().centerx
+        textpos.centery = surface.get_rect().centery
+        # Blit it onto the surface object
+        surface.blit(displayText,textpos)
+        #Convert to improve render time
+        surface.convert()
+        self.surface = surface
+
+
+
 
 pygame.init()
 pygame.display.init()
@@ -10,26 +42,31 @@ answerFont = pygame.font.Font("freesansbold.ttf",24)
 background = pygame.Surface(screen.get_size())
 background = background.convert()
 background.fill((0,250,0))
+
+buttonA = Button(150,150,300,300,(0,255,0),"Scoodly do bop", (0,0,0))
+background.blit(buttonA.surface,(buttonA.xpos,buttonA.ypos))
+
 screen.blit(background, (0,0))
 pygame.display.flip()
 
+
+
 ListOfQuestions = {'Question A: Yellow is what color?' : ["cheese", "blue","yellow","yesterday"]}
 
-while ListOfQuestions:
-    msgSurface = questionFont.render("Hello",True, (0,0,0))
-    answer11 = answerFont.render("Answer1", True, (0,0,0),(0,0,100))
-    screen.blit(msgSurface,(10,10))
-    screen.blit(answer11, (50,50))
+msgSurface = questionFont.render("Hello",True, (0,0,0))
+answer11 = answerFont.render("Answer1", True, (0,0,0),(0,0,100))
+screen.blit(msgSurface,(10,10))
+screen.blit(answer11, (50,50))
 
 
-    x = ListOfQuestions.popitem()
-
+x = ListOfQuestions.popitem()
+while True:
     question1 = questionFont.render(x[0], True, (0,0,0))
     answer1 = answerFont.render(x[1][0],True, (0,0,0))
     answer2 = answerFont.render(x[1][1],True, (0,0,0))
     answer3 = answerFont.render(x[1][2],True, (0,0,0))
     answer4 = answerFont.render(x[1][3],True, (0,0,0))
-    
+
     screen.blit(question1, (50,100))
     screen.blit(answer1, (50, 150))
     screen.blit(answer2, (50, 200))
@@ -43,7 +80,7 @@ while ListOfQuestions:
 
     pygame.display.update()
 
-    response = raw_input("If you want to proceed, enter 1 and hit enter")
-    if response == "1":
-        continue
-    
+# response = raw_input("If you want to proceed, enter 1 and hit enter")
+# if response == "1":
+#     continue
+
